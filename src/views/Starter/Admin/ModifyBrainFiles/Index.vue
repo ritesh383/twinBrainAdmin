4e59c92a42766eb1ebb621c4b7a7407822d0a593
Ritesh Pandey <ritesh.depixed@gmail.com>
2020-09-22 19:08:56 +0530

    add new build and create new page

--- src/views/Starter/Admin/ModifyBrainFiles/Index.vue
+++ src/views/Starter/Admin/ModifyBrainFiles/Index.vue
@@ -1,19 +1,14 @@
 <template>
 <div>
      <base-header class="">
-      <div class="row align-items-center py-4">
-        <div class="col-lg-6 col-7">
+      <div class="row align-items-center">
+        <!-- <div class="col-lg-6 col-7">
           <h6 class="h2 text-white d-inline-block mb-0">Default</h6>
           <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
             <route-bread-crumb></route-bread-crumb>
           </nav>
-        </div>
-        <!-- <div class="col-lg-6 col-5 text-right">
-          <base-button size="sm" type="neutral">New</base-button>
-          <base-button size="sm" type="neutral">Filters</base-button>
         </div> -->
       </div>
-      <!-- Card stats -->
       
     </base-header>
     <div class="container-fluid pt-5">
@@ -55,20 +50,6 @@
                         </div>
                     </template>
                 </el-table-column>
-                <!-- <el-table-column min-width="180px">
-                    <template v-slot="{row}">
-                        <el-dropdown trigger="click" class="dropdown">
-                        <span class="btn btn-sm btn-icon-only text-light">
-                        <i class="fas fa-ellipsis-v mt-2"></i>
-                        </span>
-                            <el-dropdown-menu class="dropdown-menu dropdown-menu-arrow show" slot="dropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </el-dropdown-menu>
-                        </el-dropdown>
-                    </template>
-                </el-table-column> -->
             </el-table>
 
             <!-- <div class="card-footer py-4 d-flex justify-content-end">
@@ -80,11 +61,11 @@
     </div>
 </template>
 <script>
- import RouteBreadCrumb from '@/components/Breadcrumb/RouteBreadcrumb';
+//  import RouteBreadCrumb from '@/components/Breadcrumb/RouteBreadcrumb';
   import { Table, TableColumn, DropdownMenu, DropdownItem, Dropdown} from 'element-ui'
   export default {
     components: {
-        RouteBreadCrumb,
+        // RouteBreadCrumb,
       [Table.name]: Table,
       [TableColumn.name]: TableColumn,
       [Dropdown.name]: Dropdown,
@@ -93,87 +74,11 @@
     },
     data() {
       return {
-        projects: [
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/bootstrap.jpg',
-            title: 'Argon Design System',
-            budget: '$2500 USD',
-            status: 'pending',
-            statusType: 'warning',
-            completion: 60
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/angular.jpg',
-            title: 'Angular Now UI Kit PRO',
-            budget: '$1800 USD',
-            status: 'completed',
-            statusType: 'success',
-            completion: 100
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/sketch.jpg',
-            title: 'Black Dashboard',
-            budget: '$3150 USD',
-            status: 'delayed',
-            statusType: 'danger',
-            completion: 72
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/react.jpg',
-            title: 'React Material Dashboard',
-            budget: '$4400 USD',
-            status: 'on schedule',
-            statusType: 'info',
-            completion: 90
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/vue.jpg',
-            title: 'Vue Paper UI Kit PRO',
-            budget: '$2200 USD',
-            status: 'completed',
-            statusType: 'success',
-            completion: 100
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/bootstrap.jpg',
-            title: 'Argon Design System',
-            budget: '$2500 USD',
-            status: 'pending',
-            statusType: 'warning',
-            completion: 60
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/angular.jpg',
-            title: 'Angular Now UI Kit PRO',
-            budget: '$1800 USD',
-            status: 'completed',
-            statusType: 'success',
-            completion: 100
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/sketch.jpg',
-            title: 'Black Dashboard',
-            budget: '$3150 USD',
-            status: 'delayed',
-            statusType: 'danger',
-            completion: 72
-          },
-          {
-            img: 'https://demos.creative-tim.com/vue-argon-dashboard-pro/img/theme/vue.jpg',
-            title: 'Vue Paper UI Kit PRO',
-            budget: '$2200 USD',
-            status: 'completed',
-            statusType: 'success',
-            completion: 100
-          }
-        ],
-        currentPage: 1,
+        // currentPage: 1,
         data: []
       };
     },
     created() {
-
-        // console.log('id',)
          let token = localStorage.getItem('token');
             // console.log(token);
             let config = {
@@ -214,7 +119,6 @@
     methods: {
         rowClicked(selectedRows) {
             // console.log('selectedRows', selectedRows)
-            // this.$router.push(`/blog-details/${blog.id}`);
             this.$router.push(`/admin/modify/${selectedRows.id}`);
         }
     }
--- src/views/Starter/Admin/ModifyBrainFiles/ModifyDetails.vue
+++ src/views/Starter/Admin/ModifyBrainFiles/ModifyDetails.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
     <base-header class="">
-      <div class="row align-items-center py-4">
-        <div class="col-lg-6 col-7">
+      <div class="row align-items-center">
+        <!-- <div class="col-lg-6 col-7">
           <h6 class="h2 text-white d-inline-block mb-0">Default</h6>
           <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
             <route-bread-crumb></route-bread-crumb>
           </nav>
-        </div>
-        <!-- <div class="col-lg-6 col-5 text-right">
-          <base-button size="sm" type="neutral">New</base-button>
-          <base-button size="sm" type="neutral">Filters</base-button>
         </div> -->
       </div>
-      <!-- Card stats -->
       
     </base-header>
     <div class="container-fluid">
@@ -96,13 +91,13 @@
   </div>
 </template>
 <script>
-  import RouteBreadCrumb from '@/components/Breadcrumb/RouteBreadcrumb';
+  // import RouteBreadCrumb from '@/components/Breadcrumb/RouteBreadcrumb';
 
 
   export default {
     name: 'Dashboard',
     components: {
-      RouteBreadCrumb
+      // RouteBreadCrumb
     },
     data() {
       return {
